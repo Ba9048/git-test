a0dcf5162d078dd8571f4328ff49c1e2ff1a6938
Ba <1592400274@qq.com>
2021-08-17 11:20:24 +0800

    first commot

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author ：Ba
+ * @date ：Created in 2021/8/17 11:15
+ * @description：
+ * @modified By：
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
